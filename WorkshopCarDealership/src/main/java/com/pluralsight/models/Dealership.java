448e1dea52f929055a2b08b95a4835041d9ba693
Daisygu19 <dgonzalez@appdev.yearup.org>
2024-02-18 18:00:29 -0500

    Added code to Dealership.Java and Vehicles.Java

--- WorkshopCarDealership/src/main/java/com/pluralsight/models/Dealership.java
+++ WorkshopCarDealership/src/main/java/com/pluralsight/models/Dealership.java
@@ -15,4 +15,80 @@ public class Dealership
         this.phone = phone;
         this.vehicles = new ArrayList<>();
     }
+
+    public ArrayList<Vehicle> getVehicles; {
+    }
+
+    public void setVehicles(ArrayList<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+    }
+
+    public String getVehiclesByMakeModel(String make, String model) {
+        ArrayList<Vehicle> matchingVehicles = new ArrayList<>();
+        for (Vehicle vehicle : vehicles) {
+            if (vehicle.getMake().equalsIgnoreCase(make) && vehicle.getModel().equalsIgnoreCase(model)) {
+                matchingVehicles.add(vehicle);
+            }
+        }
+        StringBuilder matchingVehicle = new StringBuilder();
+        for (Vehicle vehicle : matchingVehicles) {
+            matchingVehicle.append(vehicle.toString()).append(" ");
+        }
+        return matchingVehicle.toString();
+    }
+
+    public  ArrayList<Vehicle> getVehiclesByYear(int year) {
+        ArrayList<Vehicle> matchingVehicles = new ArrayList<>();
+        for (Vehicle vehicle : vehicles) {
+            if (vehicle.getYear() == year) {
+                matchingVehicles.add(vehicle);
+            }
+        }
+        return matchingVehicles;
+    }
+
+    public  ArrayList<Vehicle> getVehiclesByColor(String color) {
+        ArrayList<Vehicle> matchingVehicles = new ArrayList<>();
+        for (Vehicle vehicle : vehicles) {
+            if (vehicle.getColor().equalsIgnoreCase(color)) {
+                matchingVehicles.add(vehicle);
+            }
+        }
+        return matchingVehicles;
+    }
+
+    //public  ArrayList<Vehicle> getVehiclesByMileage(int maxMileage) {
+        //ArrayList<Vehicle> matchingVehicles = new ArrayList<>();
+        //for (Vehicle vehicle : vehicles) {
+            //if (vehicle.getMiles() <= maxMileage) {
+                //matchingVehicles.add(vehicle);
+            //}
+        //}
+        //return matchingVehicles;
+    //}
+
+    public  ArrayList<Vehicle> getVehiclesByType(String type) {
+        ArrayList<Vehicle> matchingVehicles = new ArrayList<>();
+        for (Vehicle vehicle : vehicles) {
+            if (vehicle.getType().equalsIgnoreCase(type)) {
+                matchingVehicles.add(vehicle);
+            }
+        }
+        return matchingVehicles;
+    }
+
+    public  String getAllVehicles() {
+        return null;
+    }
+    public void addVehicle(Vehicle vehicle) {
+        vehicles.add(vehicle);
+    }
+    public void removeVehicles(){
+    }
+
+
+
 }
+
+
+
